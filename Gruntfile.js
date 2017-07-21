005a606527466a6fb84aa493141be565be0ee375
cookle <cooklecurry@gmail.com>
2017-07-22 01:23:27 +0900

    Update not to delete thread tree in mail list.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 			xpi: {
 				files: [{
 					src: ['dist/custom_address_sidebar.zip'],
-					dest: 'dist/custom_address_sidebar-2.1.xpi'
+					dest: 'dist/custom_address_sidebar-2.2.xpi'
 				}]
 			}
 		}
